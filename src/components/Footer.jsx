f2d801af6d02f4ae477c490f7aee08092be3362f
Volodymyr Shynkarov <3dmirok@gmail.com>
2023-09-28 13:08:19 -0500

    edited pathes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,19 @@ const Footer = ({ children }) => {
   return (
     <div className="relative">
       <div className="absolute left-0 bottom-0">
-        <img src="../public/static/bottom_cake_bg.png" alt="" />
+        <img
+          src="https://storage.cloud.google.com/senka_logos/static/bottom_cake_bg.png"
+          alt=""
+        />
       </div>
       <div className="h-[118px] w-screen bg-[#fcc998]"></div>
-      <div className="absolute l-0 w-screen p-2 bg-[#5c4b44] text-[#f8d0a1]">
-        {new Date().getFullYear()} &copy; Volodymyr Shynkarov
+      <div className="absolute bottom-0 pl-3 pb-2 text-[#602213]">
+        <p className="z-[2]">
+          {new Date().getFullYear()} &copy;{" "}
+          <a href="https://github.com/VoldemLive/senka-ai-logo">
+            Volodymyr Shynkarov
+          </a>
+        </p>
       </div>
     </div>
   )
